a6330b9f1ca64f456609f674997c0a837e352816
murphtech <smurphy@murphy.technology>
2018-02-21 18:04:34 -0700

    Removing all demos

--- FreeRTOS/Demo/PIC32MZ_MPLAB/RTOSDemo.X/build/PIC32MZ2048_SK/production/_ext/1472/ISRTriggeredTask_isr.o.d
+++
@@ -1,9 +0,0 @@
-build/PIC32MZ2048_SK/production/_ext/1472/ISRTriggeredTask_isr.o:  \
- ../ISRTriggeredTask_isr.S  \
- ../../../Source/portable/MPLAB/PIC32MZ/ISR_Support.h  \
-../FreeRTOSConfig.h 
-build/PIC32MZ2048_SK/production/_ext/1472/ISRTriggeredTask_isr.o:  \
- ../FreeRTOSConfig.h  \
- ../../../Source/portable/MPLAB/PIC32MZ/ISR_Support.h  \
-  \
-../ISRTriggeredTask_isr.S  \
--- FreeRTOS/Demo/PIC32MZ_MPLAB/RTOSDemo.X/build/PIC32MZ2048_SK/production/_ext/1472/RegisterTestTasks.o.d
+++
@@ -1,7 +0,0 @@
-build/PIC32MZ2048_SK/production/_ext/1472/RegisterTestTasks.o:  \
- ../RegisterTestTasks.S  \
-../FreeRTOSConfig.h 
-build/PIC32MZ2048_SK/production/_ext/1472/RegisterTestTasks.o:  \
- ../FreeRTOSConfig.h  \
-  \
-../RegisterTestTasks.S  \
